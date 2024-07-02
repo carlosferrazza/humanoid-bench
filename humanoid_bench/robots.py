e3dbfba42b16deddb2f195d2d82f7a39aed854de
Carlo Sferrazza <csferrazza@berkeley.edu>
2024-07-01 19:32:43 -0700

    Fixed joint vel length

--- humanoid_bench/robots.py
+++ humanoid_bench/robots.py
@@ -43,7 +43,7 @@ class H1:
 
     def joint_velocities(self):
         """Returns the joint velocities."""
-        return self._env.data.qvel[6 : self.dof].copy()
+        return self._env.data.qvel[6 : self.dof-1].copy()
 
     def control(self):
         """Returns a copy of the control signals for the actuators."""
